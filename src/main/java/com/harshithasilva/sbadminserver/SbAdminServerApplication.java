a10ce5476d247fca42ecc7cb0568b8c21792246d
Harshitha de Silva <harshdusilves@gmail.com>
2018-11-22 10:22:03 +0530

    Enable admin server

--- src/main/java/com/harshithasilva/sbadminserver/SbAdminServerApplication.java
+++ src/main/java/com/harshithasilva/sbadminserver/SbAdminServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SbAdminServerApplication {
 
 	public static void main(String[] args) {
