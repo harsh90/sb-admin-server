42415ee0a654c453e550fc2275961d8e93bd78b4
Harshitha de Silva <harshdusilves@gmail.com>
2018-11-22 10:05:39 +0530

    Create spring boot admin server

---
+++ src/main/java/com/harshithasilva/sbadminserver/SbAdminServerApplication.java
@@ -0,0 +1,12 @@
+package com.harshithasilva.sbadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAdminServerApplication.class, args);
+	}
+}
